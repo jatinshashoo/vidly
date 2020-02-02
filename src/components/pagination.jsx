7667e44a80cb6f2d155757a0f2bb79b75a9d979d
Jatin <jatin.shashoo@gmail.com>
2020-02-02 22:40:27 +0530

    Commit till - Handling Page changes - CWM

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import _ from "lodash";
 
 const Pagination = props => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, onPageChange, currentPage } = props;
+  console.log(currentPage);
+
   const pageCount = Math.ceil(itemsCount / pageSize);
   if (pageCount === 1) return null;
   const pages = _.range(1, pageCount + 1);
@@ -10,8 +12,13 @@ const Pagination = props => {
   return (
     <nav className="ul pagination">
       {pages.map(page => (
-        <li key={page} className="page-item">
-          <a className="page-link">{page}</a>
+        <li
+          key={page}
+          className={page === currentPage ? "page-item active" : "page-item"}
+        >
+          <a onClick={() => onPageChange(page)} className="page-link">
+            {page}
+          </a>
         </li>
       ))}
     </nav>
