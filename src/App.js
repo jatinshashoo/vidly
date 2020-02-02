c8ac3d3f052aaa378e2a3970d2bb2b21616af2df
Jatin <jatin.shashoo@gmail.com>
2020-02-02 16:39:30 +0530

    Commited till -Exercise Like Component - CWM

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
-import { getMovies } from "./services/fakeMovieService";
 
 class App extends Component {
-  state = {
-    movies: getMovies()
-  };
-
-  handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
-    this.setState({ movies });
-  };
-
   render() {
     return (
       <main className="container">
-        <Movies movies={this.state.movies} onDelete={this.handleDelete} />
+        <Movies />
       </main>
     );
   }
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import Like from "./common/like";
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
   render() {
-    const { length: count } = this.props.movies;
+    const { length: count } = this.state.movies;
     if (count === 0) return <p>"There are no movies in the database"</p>;
 
     return (
@@ -20,18 +38,21 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.props.movies.map(movie => (
+            {this.state.movies.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
-                  <i class="fa fa-heart-o" aria-hidden="true"></i>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
                 </td>
                 <td>
                   <button
-                    onClick={() => this.props.onDelete(movie)}
+                    onClick={() => this.handleDelete(movie)}
                     className="btn btn-danger btn-sm"
                   >
                     Delete
