6805cdac562ed7bcbbf8041ba72230a4ee7d3ec3
Jatin <jatin.shashoo@gmail.com>
2020-02-04 21:58:26 +0530

    Committed till - Adding the sort icon - CWM

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -42,27 +42,6 @@ class MoviesTable extends Component {
           data={movies}
           columns={this.columns}
         />
-        {/* <tbody>
-          {movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like liked={movie.liked} onClick={() => onLike(movie)} />
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(movie)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody> */}
       </table>
     );
   }
