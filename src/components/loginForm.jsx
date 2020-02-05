413f7415fc0c357f23c2da4c123be1c9f6ff1cf1
Jatin <jatin.shashoo@gmail.com>
2020-02-06 01:06:30 +0530

    Committed till - Common Errors - CWM

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,23 +1,30 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
+  state = {
+    account: {
+      username: "",
+      password: ""
+    }
+  };
 
-  componentDidMount() {
-    this.username.current.focus();
-  }
+  username = React.createRef();
 
   handleSubmit = e => {
     e.preventDefault();
 
     //Call the server
     console.log("Submitted");
+  };
 
-    const username = this.username.current.value;
-    console.log("Username - ", username);
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -25,16 +32,24 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              autoFocus
-              ref={this.username}
+              onChange={this.handleChange}
+              value={account.username}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              onChange={this.handleChange}
+              value={account.password}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
