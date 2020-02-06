85702094bd9d96906a6df512eef8ff9f65552eed
Jatin <jatin.shashoo@gmail.com>
2020-02-06 23:17:49 +0530

    Committed till - Displaying validation errors -CWM

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,17 +2,34 @@ import React, { Component } from "react";
 import Input from "./common/input";
 
 class LoginForm extends Component {
+  username = React.createRef();
+
   state = {
     account: {
       username: "",
       password: ""
-    }
+    },
+    errors: {}
   };
 
-  username = React.createRef();
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
 
   handleSubmit = e => {
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+
+    if (errors) return;
 
     //Call the server
     console.log("Submitted");
@@ -25,7 +42,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -35,12 +52,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
