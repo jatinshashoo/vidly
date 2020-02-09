9629e9c9f17c982326632876131d34c910e3a1d0
Jatin <jatin.shashoo@gmail.com>
2020-02-09 18:39:58 +0530

    Committed till - Extracting a config file - CWM

--- src/services/movieService.jsx
+++ src/services/movieService.jsx
@@ -1,11 +1,12 @@
 import http from "./httpservice";
+import { apiUrl } from "../config.json";
 
 const apiEndpoint = "http://localhost:3900/api/movies";
 
 export function getMovies() {
-  return http.get(apiEndpoint);
+  return http.get(`${apiUrl}/movies`);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(`${apiEndpoint}/${movieId}`);
+  return http.delete(`${apiUrl}/movies/${movieId}`);
 }
